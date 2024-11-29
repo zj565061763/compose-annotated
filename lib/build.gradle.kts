4aff5c035d0daaf1e9ea1ef5841bddeb7cb5cea4
zjun <565061763@qq.com>
2024-11-29 16:30:28 +0800

    1.1.0-rc01

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 val libGroupId = "com.sd.lib.android"
 val libArtifactId = "compose-annotated"
-val libVersion = "1.1.0-beta05"
+val libVersion = "1.1.0-rc01"
 
 android {
    namespace = "com.sd.lib.compose.annotated"
